a0b37e136b5a222b6089e6881e16d6c79160318b
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-04-23 10:23:14 +0200

    update colors

--- colors.js
+++ colors.js
@@ -5,11 +5,11 @@ module.exports.colors = {
     "shade0": "#343637",
     "shade1": "#495A64",
     "shade2": "#556873",
-    "shade3": "#8FABBD",
-    "shade4": "#899BA6",
-    "shade5": "#A7B8C2",
-    "shade6": "#CAE2F2",
-    "shade7": "#F2F2F2",
+    "shade3": "#A7C2D3",
+    "shade4": "#BFCCD4",
+    "shade5": "#C5D1D8",
+    "shade6": "#E3F2FD",
+    "shade7": "#FFFFFF",
     "accent0": "#E66565",
     "accent1": "#F2C38F",
     "accent2": "#FFFA70",
@@ -21,4 +21,4 @@ module.exports.colors = {
   }
 };
 
-// Your theme's URL: https://themer.dev/?colors.dark.shade0=%23343637&colors.dark.shade1=%23495A64&colors.dark.shade2=%23556873&colors.dark.shade3=%238fabbd&colors.dark.shade4=%23899BA6&colors.dark.shade5=%23A7B8C2&colors.dark.shade6=%23cae2f2&colors.dark.shade7=%23f2f2f2&colors.dark.accent0=%23e66565&colors.dark.accent1=%23F2C38F&colors.dark.accent2=%23fffa70&colors.dark.accent3=%2392dd69&colors.dark.accent4=%237FC1CA&colors.dark.accent5=%2383AFE5&colors.dark.accent6=%23ff944d&colors.dark.accent7=%23ff8ab3&activeColorSet=dark&calculateIntermediaryShades.dark=false&calculateIntermediaryShades.light=true
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%23343637&colors.dark.shade1=%23495A64&colors.dark.shade2=%23556873&colors.dark.shade3=%23a7c2d3&colors.dark.shade4=%23bfccd4&colors.dark.shade5=%23c5d1d8&colors.dark.shade6=%23e3f2fd&colors.dark.shade7=%23ffffff&colors.dark.accent0=%23e66565&colors.dark.accent1=%23F2C38F&colors.dark.accent2=%23fffa70&colors.dark.accent3=%2392dd69&colors.dark.accent4=%237FC1CA&colors.dark.accent5=%2383AFE5&colors.dark.accent6=%23ff944d&colors.dark.accent7=%23ff8ab3&activeColorSet=dark&calculateIntermediaryShades.dark=false&calculateIntermediaryShades.light=true
